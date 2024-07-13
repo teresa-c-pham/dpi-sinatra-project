f187091c3aa6f69d14bda990a64a9e93d61ddcb3
Teresa Pham <79329784+teresa-c-pham@users.noreply.github.com>
2024-07-13 00:04:05 +0000

    Implement duck api

--- app.rb
+++ app.rb
@@ -9,12 +9,15 @@ end
 
 get("/generate") do
   @outcome = params.fetch("animal")
-  random = ["Cat", "Dog", "Fox"].sample
+  random = ["Cat", "Dog", "Fox", "Duck"].sample
 
   # List of APIs
   cat_api = "https://api.thecatapi.com/v1/images/search"
   fox_api = "https://randomfox.ca/floof/?ref=apilist.fun"
   dog_api = "https://dog.ceo/api/breeds/image/random"
+  duck_api = "https://random-d.uk/api/random"
+
+  
   # Check if random was selected
   if @outcome == "Surprise"
     @outcome = random
@@ -22,7 +25,7 @@ get("/generate") do
 
   # Call Different API for each animal type
   if @outcome == "Cat"
-    @message = ["Cats!", "More Cats!", "Even More Cats!", "I did it", "Don't I Look Cute?"].sample
+    @message = ["Cats!", "More Cats!", "Even More Cats!", "I did it", "Don't I Look Cute?", "Meow~"].sample
     cat = HTTP.get(cat_api).to_s
     cat_data = JSON.parse(cat)
     data = cat_data[0]
@@ -34,10 +37,15 @@ get("/generate") do
     fox_data = JSON.parse(fox)
     @img = fox_data["image"]
   elsif @outcome == "Dog"
-    @message = ["I'm a good boy", "Happy~", "Lookin' smug", "More Doggos!", "Even More Doggo Pics!", "Doggos!"].sample
+    @message = ["I'm a good boy", "Happy~", "Lookin' smug", "More Doggos!", "Even More Doggo Pics!", "Doggos!", "Woof"].sample
     dog = HTTP.get(dog_api).to_s
     dog_data = JSON.parse(dog)
     @img = dog_data["message"]
+  else
+    @message = ["I'm a duck", "Fluffy Duck", "Funky Duck", "Quack", "Quack Quack"].sample
+    duck = HTTP.get(duck_api).to_s
+    duck_data = JSON.parse(duck)
+    @img = duck_data["url"]
   end
 
   erb(:generate)
